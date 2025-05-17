9923c917235f867a6add120bd57252982a30bf25
Faizan Rashid <faizanrashid@Faizans-MacBook-Air.local>
2025-05-16 23:54:07 -0400

    Initial commit of kobweb app

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-# traininghub
-Personal dashboard for your training data.
+# Training Hub
+Personal dashboard for your training data. Initially the focus is going to be on running data from Garmin.
+
+## Dashboard(s)
+
+## Data Privacy
+
+
+
+
---
+++ site/src/jsMain/kotlin/io/github/faizanrashid91/traininghub/components/widgets/IconButton.kt
@@ -0,0 +1,21 @@
+package io.github.faizanrashid91.traininghub.components.widgets
+
+import androidx.compose.runtime.Composable
+import com.varabyte.kobweb.compose.ui.Modifier
+import com.varabyte.kobweb.compose.ui.modifiers.setVariable
+import com.varabyte.kobweb.silk.components.forms.Button
+import com.varabyte.kobweb.silk.components.forms.ButtonVars
+import org.jetbrains.compose.web.css.em
+import io.github.faizanrashid91.traininghub.CircleButtonVariant
+import io.github.faizanrashid91.traininghub.UncoloredButtonVariant
+
+@Composable
+fun IconButton(onClick: () -> Unit, content: @Composable () -> Unit) {
+    Button(
+        onClick = { onClick() },
+        Modifier.setVariable(ButtonVars.FontSize, 1.em), // Make button icon size relative to parent container font size
+        variant = CircleButtonVariant.then(UncoloredButtonVariant)
+    ) {
+        content()
+    }
+}
