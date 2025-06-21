57dbf25c4e15a9022d450f587aad5c7fd68eb45f
Faizan Rashid <faizanrashid@Faizans-MacBook-Air.local>
2025-06-21 11:51:50 -0500

    Clean-up the template so we can build on top of this

---
+++ site/src/jsMain/kotlin/io/github/faizanrashid91/traininghub/pages/Dashboard.kt
@@ -0,0 +1,41 @@
+package io.github.faizanrashid91.traininghub.pages
+
+import androidx.compose.runtime.Composable
+import com.varabyte.kobweb.compose.css.StyleVariable
+import com.varabyte.kobweb.compose.foundation.layout.Box
+import com.varabyte.kobweb.compose.foundation.layout.Column
+import com.varabyte.kobweb.compose.foundation.layout.Row
+import com.varabyte.kobweb.compose.ui.Modifier
+import com.varabyte.kobweb.compose.ui.graphics.Color
+import com.varabyte.kobweb.compose.ui.graphics.Colors
+import com.varabyte.kobweb.compose.ui.modifiers.*
+import com.varabyte.kobweb.compose.ui.toAttrs
+import com.varabyte.kobweb.core.Page
+import com.varabyte.kobweb.core.rememberPageContext
+import com.varabyte.kobweb.silk.components.forms.Button
+import com.varabyte.kobweb.silk.components.navigation.Link
+import com.varabyte.kobweb.silk.components.text.SpanText
+import com.varabyte.kobweb.silk.style.CssStyle
+import com.varabyte.kobweb.silk.style.base
+import com.varabyte.kobweb.silk.style.breakpoint.Breakpoint
+import com.varabyte.kobweb.silk.style.breakpoint.displayIfAtLeast
+import com.varabyte.kobweb.silk.style.toAttrs
+import com.varabyte.kobweb.silk.style.toModifier
+import com.varabyte.kobweb.silk.theme.colors.ColorMode
+import com.varabyte.kobweb.silk.theme.colors.ColorPalettes
+import org.jetbrains.compose.web.css.cssRem
+import org.jetbrains.compose.web.css.fr
+import org.jetbrains.compose.web.css.px
+import org.jetbrains.compose.web.css.vh
+import org.jetbrains.compose.web.dom.Div
+import org.jetbrains.compose.web.dom.Text
+import io.github.faizanrashid91.traininghub.HeadlineTextStyle
+import io.github.faizanrashid91.traininghub.SubheadlineTextStyle
+import io.github.faizanrashid91.traininghub.components.layouts.PageLayout
+import io.github.faizanrashid91.traininghub.toSitePalette
+
+@Page
+@Composable
+fun Dashboard() {
+    
+}
